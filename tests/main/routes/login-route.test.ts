bc3cae1d14d085f3992127b8d42b266122812600
Andre Alves <andrecambridge@gmail.com>
2021-02-22 12:50:38 -0300

    test: ensure login route returns 200 on login

---
+++ tests/main/routes/login-route.test.ts
@@ -0,0 +1,38 @@
+import { MongoHelper } from "@/infra/db"
+import app from '@/app'
+import { hash } from 'bcrypt'
+import { Collection } from "mongodb"
+import request from 'supertest'
+
+let userCollection: Collection
+
+describe('POST login', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    userCollection = await MongoHelper.getCollection('users')
+    await userCollection.deleteMany({})
+  })
+
+  test('Should return 200 on login', async () => {
+    const password = await hash('123456', 12)
+    await userCollection.insertOne({
+      name: 'User One',
+      email: "userone@users.com",
+      password
+    })
+    await request(app)
+      .post('/login')
+      .send({
+        email: 'userone@users.com',
+        password: '123456'
+      })
+      .expect(200)
+  })
+})
