54ec891d53ef56e35b1e6ffe93ab26b7d2f3d189
Andre Alves <andrecambridge@gmail.com>
2021-02-23 12:34:23 -0300

    feat: ensure bcrypt adapter is called with correct values

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,7 @@
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter {
+  async compare(plainText: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plainText, digest)
+  }
+}
