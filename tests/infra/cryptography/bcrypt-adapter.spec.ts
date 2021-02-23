3a0710ea35d7e130eb210bd1e4c1cac568e4971f
Andre Alves <andrecambridge@gmail.com>
2021-02-23 12:34:42 -0300

    test: ensure bcrypt adapter is called with correct values

---
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,28 @@
+import { BcryptAdapter } from "@/infra/cryptography"
+import bcrypt from 'bcrypt'
+
+jest.mock('bcrypt', () => ({
+  async compare(): Promise<boolean> {
+    return true
+  }
+}))
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter()
+}
+
+describe('Bcrypt Adapter', () => {
+  test('Should call compare with correct values', async () => {
+    const sut = makeSut()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.compare('any_plainText', 'any_digest')
+    expect(compareSpy).toHaveBeenCalledWith('any_plainText', 'any_digest')
+  })
+
+  test('Should return false when compare fails', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(false)
+    const isValid = await sut.compare('any_plainText', 'any_digest')
+    expect(isValid).toBe(false)
+  })
+})
