99c0cf3152e125270b35d61eb1b5bef38b585ca0
Andre Alves <andrecambridge@gmail.com>
2021-02-24 11:54:24 -0300

    refactor: adjust Login Controller tests after Authentication refactor

--- tests/presentation/controllers/login-controller.spec.ts
+++ tests/presentation/controllers/login-controller.spec.ts
@@ -1,11 +1,7 @@
 import { LoginController } from '@/presentation/controllers'
-import { MongoHelper } from '@/infra/db'
-import { Collection } from 'mongodb'
-import { serverError, unauthorized } from '@/presentation/helpers'
-import { throwError } from '@/tests/domain/mocks/test-helpers'
-import { AuthenticationSpy } from '../mocks/mock-account'
-
-let userCollection: Collection
+import { serverError, success, unauthorized } from '@/presentation/helpers'
+import { throwError } from '@/tests/domain/mocks'
+import { AuthenticationSpy } from '@/tests/presentation/mocks'
 
 const mockRequest = (): LoginController.Request => ({
   email: 'any_email@mail.com',
@@ -27,26 +23,13 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Login Controller', () => {
-  beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL)
-  })
-
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
-
-  beforeEach(async () => {
-    userCollection = await MongoHelper.getCollection('users')
-    await userCollection.deleteMany({})
-  })
-
-  test('Should call controller with correct values', async () => {
-    const { sut } = makeSut()
-    const handleSpy = jest.spyOn(sut, 'handle')
-    await sut.handle(mockRequest())
-    expect(handleSpy).toHaveBeenCalledWith({
-      email: 'any_email@mail.com',
-      password: 'any_password'
+  test('Should call Authentication with correct values', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(authenticationSpy.params).toEqual({
+      email: request.email,
+      password: request.password
     })
   })
 
@@ -60,17 +43,16 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(unauthorized())
   })
 
-  test('Should return 401 if wrong password is provided', async () => {
-    const { sut, authenticationSpy } = makeSut()
-    authenticationSpy.result = null
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(unauthorized())
-  })
-
-  test('Should return 500 if LoginController throws', async () => {
+  test('Should return 500 if Authentication throws', async () => {
     const { sut, authenticationSpy } = makeSut()
     jest.spyOn(authenticationSpy, 'auth').mockImplementationOnce(throwError)
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 if valid credentials are provided', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(success(authenticationSpy.result))
   })
 })
