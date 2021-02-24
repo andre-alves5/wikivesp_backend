4d3251416c8df7e33a0332809639ae76b7dc7fe9
Andre Alves <andrecambridge@gmail.com>
2021-02-24 15:38:21 -0300

    fix: security issues

--- src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: '272A63C9E9C5D34A99D6D949A163CBD0',
-  expiresIn: '1d'
-}
--- src/config/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  url: 'http://localhost:8080',
-  urlSite: 'http://localhost:3000'
-}
