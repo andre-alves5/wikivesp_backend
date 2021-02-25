98b1468243ac4750ffd60c3f4d07e1b1b46c86bc
Andre Alves <andrecambridge@gmail.com>
2021-02-25 12:05:06 -0300

    test: ensure SignUpController calls AddAccount with correct values

--- tests/presentation/mocks/mock-account.ts
+++ tests/presentation/mocks/mock-account.ts
@@ -1,5 +1,15 @@
-import { Authentication } from '@/domain/usecases'
+import { AddAccount, Authentication } from '@/domain/usecases'
 import faker from 'faker'
+
+export class AddAccountSpy implements AddAccount {
+  params: AddAccount.Params
+  result = true
+
+  async add (params: AddAccount.Params): Promise<AddAccount.Result> {
+    this.params = params
+    return this.result
+  }
+}
 
 export class AuthenticationSpy implements Authentication {
   params: Authentication.Params
