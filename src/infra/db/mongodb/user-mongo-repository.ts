213476466160787abdeefd97713365b2389cc200
Andre Alves <andrecambridge@gmail.com>
2021-02-25 16:02:45 -0300

    feat: ensure UserMongoRepository returns true if email already exists

--- src/infra/db/mongodb/user-mongo-repository.ts
+++ src/infra/db/mongodb/user-mongo-repository.ts
@@ -1,10 +1,18 @@
-import { LoadUserByEmailRepository } from '@/data/protocols'
+import { CheckAccountByEmailRepository, LoadUserByEmailRepository } from '@/data/protocols'
 import { MongoHelper } from './mongo-helper'
 
-export class UserMongoRepository implements LoadUserByEmailRepository {
+export class UserMongoRepository implements LoadUserByEmailRepository, CheckAccountByEmailRepository {
   async loadByEmail (email: string): Promise<LoadUserByEmailRepository.Result> {
     const userCollection = await MongoHelper.getCollection('users')
     const user = await userCollection.findOne({ email })
     return user && MongoHelper.map(user)
+  }
+
+  async checkByEmail (email: string): Promise<CheckAccountByEmailRepository.Result> {
+    const userCollection = await MongoHelper.getCollection('users')
+    const account = await userCollection.findOne({
+      email
+    })
+    return account !== null
   }
 }
