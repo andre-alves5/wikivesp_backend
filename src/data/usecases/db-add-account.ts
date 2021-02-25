fe66f898c593d255ace8c3748e6c9fd9c6d42a82
Andre Alves <andrecambridge@gmail.com>
2021-02-25 14:15:20 -0300

    feat: ensure DbAddAccount call CheckAccountByEmailRepository with correct values

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,13 +1,16 @@
 import { AddAccountRepository, Hasher } from '@/data/protocols'
 import { AddAccount } from '@/domain/usecases'
+import { CheckAccountByEmailRepository } from '../protocols/db/user/check-account-by-email-repository'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository
   ) { }
 
   async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
+    await this.checkAccountByEmailRepository.checkByEmail(accountData.email)
     let isValid = false
     const hashedPassword = await this.hasher.hash(accountData.password)
     isValid = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
