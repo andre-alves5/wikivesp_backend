8ec2fd22610bcc5cd5758e3b0c6f6fbb77dd8b33
Andre Alves <andrecambridge@gmail.com>
2021-02-25 14:01:58 -0300

    test: ensure DbAddAccount call AddAccountRepository with correct values

--- tests/data/mocks/mock-db-account.ts
+++ tests/data/mocks/mock-db-account.ts
@@ -1,5 +1,15 @@
-import { LoadUserByEmailRepository } from '@/data/protocols'
+import { AddAccountRepository, LoadUserByEmailRepository } from '@/data/protocols'
 import faker from 'faker'
+
+export class AddAccountRepositorySpy implements AddAccountRepository {
+  params: AddAccountRepository.Params
+  result = true
+
+  async add (params: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
+    this.params = params
+    return this.result
+  }
+}
 
 export class LoadUserByEmailRepositorySpy implements LoadUserByEmailRepository {
   email: string
